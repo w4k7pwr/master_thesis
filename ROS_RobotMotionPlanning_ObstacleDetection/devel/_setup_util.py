3fe661503249137445dec642aed18665831cd3ea
wasiel13 <mateusz.wasielewski13@gmail.com>
2016-03-06 01:09:49 +0100

    ROS Indigo and Gazebo2 configuration

--- ROS_RobotMotionPlanning_ObstacleDetection/devel/_setup_util.py
+++ ROS_RobotMotionPlanning_ObstacleDetection/devel/_setup_util.py
@@ -260,7 +260,7 @@ if __name__ == '__main__':
             sys.exit(1)
 
         # environment at generation time
-        CMAKE_PREFIX_PATH = '/opt/ros/indigo'.split(';')
+        CMAKE_PREFIX_PATH = '/home/wasiel13/wasiel_13/SYNERGIA_PROJECTS/master_thesis/ROS_RobotMotionPlanning_ObstacleDetection/devel;/opt/ros/indigo'.split(';')
         # prepend current workspace if not already part of CPP
         base_path = os.path.dirname(__file__)
         if base_path not in CMAKE_PREFIX_PATH:
