5cc0986f3253a536492a9f04821ee6e1d871efd2
wasiel13 <mateusz.wasielewski13@gmail.com>
2016-03-12 22:24:22 +0100

    clean directory

--- ROS_RobotMotionPlaning_ObstacleDetection/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/wasiel13/wasiel_13/SYNERGIA_PROJECTS/master_thesis/ROS_RobotMotionPlaning_ObstacleDetection/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/wasiel13/wasiel_13/SYNERGIA_PROJECTS/master_thesis/ROS_RobotMotionPlaning_ObstacleDetection/devel;/opt/ros/indigo".split(';') if "/home/wasiel13/wasiel_13/SYNERGIA_PROJECTS/master_thesis/ROS_RobotMotionPlaning_ObstacleDetection/devel;/opt/ros/indigo" != "" else []
--- ROS_RobotMotionPlaning_ObstacleDetection/build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/wasiel13/wasiel_13/SYNERGIA_PROJECTS/master_thesis/ROS_RobotMotionPlaning_ObstacleDetection/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
